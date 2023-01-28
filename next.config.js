60310f5ba2a3c02da962bff91381f95a81cc06ad
Sayedhassan-Hussaini <sh.hussaini1378@gmail.com>
2023-01-28 15:09:29 +0430

    setup config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const withOffline = require("next-offline");
 
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  },
 };
 
 module.exports = withOffline(nextConfig);
